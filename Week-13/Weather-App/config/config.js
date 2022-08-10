0dd474cc3f13a76e1fb6a447eb110bb8e753c332
Ashish <105300000+ShadYBhai@users.noreply.github.com>
2022-08-10 16:42:14 +0530

    Create config.js

---
+++ Week-13/Weather-App/config/config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap : {
+        BASE_URL : "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"c1d6b38deb9fa62c128b88fa4dce9e42"
+    }
+}
+
+module.exports = constants
