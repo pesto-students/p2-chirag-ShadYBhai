b596b2829e8d8be86310fe216d5f36d3c4bb18b4
Ashish <105300000+ShadYBhai@users.noreply.github.com>
2022-08-10 16:42:48 +0530

    Create configF.js

---
+++ Week-13/Weather-App/config/configF.js
@@ -0,0 +1,9 @@
+const constantsF = {
+    openWeatherMap : {
+        BASE_URL : "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"c1d6b38deb9fa62c128b88fa4dce9e42"
+    }
+}
+// https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={API key}  // FORCASRT FOR PRO VERSION
+// api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
+module.exports = constantsF
