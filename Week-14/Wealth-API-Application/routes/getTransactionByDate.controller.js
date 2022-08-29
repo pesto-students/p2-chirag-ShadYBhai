47dd5b05c0c4c29082d9acac157c8ee692ee4791
Ashish <105300000+ShadYBhai@users.noreply.github.com>
2022-08-29 22:19:30 +0530

    Update getTransactionByDate.controller.js

--- Week-14/Wealth-API-Application/routes/getTransactionByDate.controller.js
+++ Week-14/Wealth-API-Application/routes/getTransactionByDate.controller.js
@@ -1,1 +1,20 @@
+const createConnection = require('./connection')
 
+const getTransactionByDate = (params,callback) => {
+    const {created} = params
+
+    const connection = createConnection()
+
+    const query = `select asset_id, amount, amount_type_id from transaction where created='${created}';`
+
+    connection.query(query, (err, result)=>{
+        if(result){
+            callback({FoundOnGivenDate: true})
+        }
+        else if(err){
+            callback({FoundOnGivenDate : false})
+        }
+    })
+}
+
+module.exports = getTransactionByDate
